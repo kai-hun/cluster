435e0820d23995e9461966f509a43ce9f8ae3b9a
kai-hun <68891654+kai-hun@users.noreply.github.com>
2023-04-10 19:39:29 +0900

    Add files via upload

--- ardouino/RGB/RGB.ino
+++ ardouino/RGB/RGB.ino
@@ -1,6 +1,9 @@
 int redPin = 9;
 int greenPin = 10;
 int bluePin = 11;
+int redValue = 0;
+int greenValue = 0;
+int blueValue = 0;
 
 void setup() {
   pinMode(redPin, OUTPUT);
@@ -9,18 +12,22 @@ void setup() {
 }
 
 void loop() {
-  analogWrite(redPin, 255);
-  analogWrite(greenPin, 0);
-  analogWrite(bluePin, 0);
-  delay(1000);
+  analogWrite(redPin, redValue);
+  analogWrite(greenPin, greenValue);
+  analogWrite(bluePin, blueValue);
+  redValue++;
+  greenValue++;
+  blueValue++;
 
-  analogWrite(redPin, 0);
-  analogWrite(greenPin, 255);
-  analogWrite(bluePin, 0);
-  delay(1000);
-
-  analogWrite(redPin, 0);
-  analogWrite(greenPin, 0);
-  analogWrite(bluePin, 255);
-  delay(1000);
+  if (redValue > 255) {
+    redValue = 0;
+  }
+  if (greenValue > 255) {
+    greenValue = 0;
+  }
+  if (blueValue > 255) {
+    blueValue = 0;
+  }
+  
+  delay(10);
 }
